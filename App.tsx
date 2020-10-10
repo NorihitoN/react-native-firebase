15d1cd8cb316fa1a02e33543617f987e534d4bf8
norihito <norihito0626@gmail.com>
2020-10-11 00:06:15 +0900

    get data from firebase

--- App.tsx
+++ App.tsx
@@ -1,11 +1,39 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import * as firebase from 'firebase';
+import "firebase/firestore";
 import { StyleSheet, Text, View } from 'react-native';
 
+/* lib */
+import { getShops } from "./src/lib/firebase";
+/* types */
+import { Shop } from "./src/types/shop";
+
 export default function App() {
+  // Ingnore setting timer warning
+  // See https://github.com/facebook/react-native/issues/12981
+  console.ignoredYellowBox = ['Setting a timer'];
+
+  const [shops, setShops] = useState<Shop[]>([]);
+  useEffect(() => {
+    getFirebaseItems();
+  }, []);
+
+  const getFirebaseItems = async () => {
+    const shops = await getShops();
+    setShops(shops);
+  };
+
+  const shopItems = shops.map((shop, index) => (
+    <View style={{margin: 10}} key={index.toString()}>
+      <Text>{shop.name}</Text>
+      <Text>{shop.place}</Text>
+    </View>
+  ));
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      {shopItems}
       <StatusBar style="auto" />
     </View>
   );
